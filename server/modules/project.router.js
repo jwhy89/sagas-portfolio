12433f0404b457b5b749c288d1995fc38f8cdb92
Jarvis Yang <jarvisgyang@gmail.com>
2019-04-14 23:13:46 -0500

    post request working with postman; will need to set up axios saga

--- server/modules/project.router.js
+++ server/modules/project.router.js
@@ -39,40 +39,28 @@ router.get('/admin', (req, res) => {
         });
 });
 
-// router.get('/details/:id', (req, res) => {
-//     const queryText = 'SELECT * FROM plant WHERE id=$1';
-//     pool.query(queryText, [req.params.id])
-//         .then((result) => {
-//             res.send(result.rows);
-//         })
-//         .catch((err) => {
-//             console.log('Error completing SELECT plant query', err);
-//             res.sendStatus(500);
-//         });
-// });
-
-// router.post('/', (req, res) => {
-//     const newPlant = req.body;
-//     const queryText = `INSERT INTO plant ("name", "kingdom", "clade", "order", "family", "subfamily", "genus")
-//                     VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-//     const queryValues = [
-//         newPlant.name,
-//         newPlant.kingdom,
-//         newPlant.clade,
-//         newPlant.order,
-//         newPlant.family,
-//         newPlant.subfamily,
-//         newPlant.genus,
-//     ];
-//     pool.query(queryText, queryValues)
-//         .then(() => {
-//             res.sendStatus(201);
-//         })
-//         .catch((err) => {
-//             console.log('Error completing SELECT plant query', err);
-//             res.sendStatus(500);
-//         });
-// });
+router.post('/', (req, res) => {
+    const newProject = req.body;
+    const queryText = `INSERT INTO projects ("name", "description", "thumbnail", "website", "github", "date_completed", "tag_id")
+                    VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+    const queryValues = [
+        newProject.name,
+        newProject.description,
+        newProject.thumbnail,
+        newProject.website,
+        newProject.github,
+        newProject.date_completed,
+        newProject.tag_id,
+    ];
+    pool.query(queryText, queryValues)
+        .then(() => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('Error completing POST projects query', err);
+            res.sendStatus(500);
+        });
+});
 
 // router.put('/', (req, res) => {
 //     const updatedPlant = req.body;
